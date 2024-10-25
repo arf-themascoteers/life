4509580bb0394114de8f4349b9224c5ed0e27118
Arif <arif@themascoteers.com>
2024-10-25 13:14:21 +1100

    done

--- housekeeping/lucas_review.py
+++ housekeeping/lucas_review.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 
-df = pd.read_csv("../data/lucas_lc0_r.csv")
+df = pd.read_csv("../data/lucas_lc0_s_r.csv")
 unique_values_with_counts = df['lc'].value_counts()
 print(unique_values_with_counts)
