39474c4734137f3a298b1d97be02c7dc5777895d
arif <arif@themascoteers.com>
2024-10-27 01:53:39 +1100

    done

--- run_all.py
+++ run_all.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     tag = "complete"
     tasks = {
         "algorithms" : ["random", "linspacer", "pcal", "mcuve", "bsnet", "bnc", "c1", "bsdr"],
-        "datasets": ["indian_pines","paviaU","salinas","ghisaconus","lucas_r"],
+        "datasets": ["indian_pines","paviaU","salinas","ghisaconus"],
         "target_sizes" : [5,10,15,20,25,30]
     }
     ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
