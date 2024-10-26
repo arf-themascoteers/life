63ceed08dfc1c21a988ab0cd46e0d850c18744ef
Arif <arif@themascoteers.com>
2024-10-26 22:58:35 +1100

    done

--- ds_manager.py
+++ ds_manager.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import RandomUnderSampler
+import numpy as np
 
 
 class DSManager:
@@ -13,6 +15,22 @@ class DSManager:
             frac = 1
         df = df.sample(frac=frac).reset_index(drop=True)
         self.data = df.to_numpy()
+        if name == "lucas_texture_4_r":
+            X = self.data[:, :-1]
+            y = self.data[:, -1]
+            rus = RandomUnderSampler(sampling_strategy={0: 173, 1: 173, 2: 173, 3: 173})
+            X_resampled, y_resampled = rus.fit_resample(X, y)
+            self.data = np.column_stack((X_resampled, y_resampled))
+            print(len(self.data))
+        if name == "lucas_lc0_s_r":
+            X = self.data[:, :-1]
+            y = self.data[:, -1]
+            rus = RandomUnderSampler(sampling_strategy={0: 599, 1: 599, 2: 599, 3: 599, 4:599})
+            X_resampled, y_resampled = rus.fit_resample(X, y)
+            self.data = np.column_stack((X_resampled, y_resampled))
+            print(len(self.data))
+
+
 
     def get_name(self):
         return self.name
