95342bdd67515c030b11f9c094f100db37d77301
Arif <arif@themascoteers.com>
2024-10-27 00:07:54 +1100

    done

--- run_bs.py
+++ run_bs.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     tag = "bs"
     tasks = {
         "algorithms" : ["bnc","bsnet"],
-        "datasets": ["indian_pines","salinas","paviaU","ghisaconus","lucas_texture_r"],
+        "datasets": ["indian_pines","salinas","paviaU","ghisaconus"],
         "target_sizes" : [5,10,15,20,25,30]
     }
     ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
