4dd9b0a6267b4fc91192fe1f3fabf2051af41c77
arif <arif@themascoteers.com>
2024-10-28 00:20:55 +1100

    done

--- run_p6_classification.py
+++ run_p6_classification.py
@@ -1,10 +1,10 @@
 from task_runner import TaskRunner
 
 if __name__ == '__main__':
-    tag = "p3"
+    tag = "p6"
     tasks = {
         "algorithms" : ["random", "linspacer", "pcal", "mcuve", "bsnet", "bnc", "c1", "bsdr","bsdrattn","c1_wo_dsc","msobsdr"],
-        "datasets": ["indian_pines","paviaU"],
+        "datasets": ["ghisaconus"],
         "target_sizes" : list(range(2,31))
     }
     ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
