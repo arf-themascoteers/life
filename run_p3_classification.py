f98e68911809db39f657d94ebd02bd3bd7bb0684
arif <arif@themascoteers.com>
2024-10-28 00:19:10 +1100

    done

--- run_p3_classification.py
+++ run_p3_classification.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     tag = "p3"
     tasks = {
         "algorithms" : ["random", "linspacer", "pcal", "mcuve", "bsnet", "bnc", "c1", "bsdr","bsdrattn","c1_wo_dsc","msobsdr"],
-        "datasets": ["indian_pines","paviaU","salinas","ghisaconus"],
+        "datasets": ["indian_pines","paviaU","ghisaconus"],
         "target_sizes" : list(range(2,31))
     }
     ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
