04134f17c3392872de19bbff957c9e81113d9ea9
Arif <arif@themascoteers.com>
2024-10-29 18:26:15 +1100

    done

--- run_p7_skiprattn.py
+++ run_p7_skiprattn.py
@@ -3,9 +3,9 @@ from task_runner import TaskRunner
 if __name__ == '__main__':
     tag = "p7"
     tasks = {
-        "algorithms" : ["skipattn"],
+        "algorithms" : ["skipattn","bsdrattn","lateattn","stage3attn"],
         "datasets": ["indian_pines","paviaU","salinas","ghisaconus","lucas_r"],
         "target_sizes" : list(range(2,31))
     }
-    ev = TaskRunner(tasks,tag,skip_all_bands=True, verbose=False, test=False)
+    ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
     summary, details = ev.evaluate()
