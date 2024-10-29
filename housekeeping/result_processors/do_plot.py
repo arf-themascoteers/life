383eb550ade07e3b407cb2145702bebe34b9c4a6
Arif <arif@themascoteers.com>
2024-10-29 18:31:56 +1100

    done

--- housekeeping/result_processors/do_plot.py
+++ housekeeping/result_processors/do_plot.py
@@ -7,4 +7,9 @@ def plot(*args, **kwargs):
     plotter_regression.plot_combined(*args, **kwargs)
 
 
-plot(only_algorithms=["bsdrattn","skipattn","lateattn","stage3attn"])
+#plot(only_algorithms=["bsdrattn","skipattn","lateattn","stage3attn"])
+#plot(only_algorithms=["bsdrattn","lateattn"])
+#plot(only_algorithms=["lateattn","stage3attn"])
+#plot(only_algorithms=["bsdrattn","stage3attn"])
+#plot(["."],only_algorithms=["bsdrattn","skipattn"], pending=True)
+plot(only_algorithms=["bsdrattn","skipattn"])
---
+++ run_m1_attn.py
@@ -0,0 +1,11 @@
+from task_runner import TaskRunner
+
+if __name__ == '__main__':
+    tag = "m1"
+    tasks = {
+        "algorithms" : ["bsdr","c1","bsdrattn"],
+        "datasets": ["indian_pines","paviaU","salinas","ghisaconus","lucas_r"],
+        "target_sizes" : list(range(2,151))
+    }
+    ev = TaskRunner(tasks,tag,skip_all_bands=True, verbose=False, test=False)
+    summary, details = ev.evaluate()
