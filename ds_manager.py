70c98b7f4b72506b6a2a740e3bd05caff0bf90fb
Arif <arif@themascoteers.com>
2024-10-29 18:18:51 +1100

    done

--- ds_manager.py
+++ ds_manager.py
@@ -3,9 +3,10 @@ from sklearn.model_selection import train_test_split
 
 
 class DSManager:
-    def __init__(self, name, test=False):
+    def __init__(self, name, test=False,split="bsnet"):
         self.name = name
         self.test = test
+        self.split = split
         dataset_path = f"data/{name}.csv"
         df = pd.read_csv(dataset_path)
         frac = 1
@@ -13,6 +14,14 @@ class DSManager:
             frac = 1
         df = df.sample(frac=frac).reset_index(drop=True)
         self.data = df.to_numpy()
+        if self.split=="bsnet":
+            self.bs_train_data = self.data
+            self.svm_evaluate_data = self.data
+        else:
+            if self.is_classification():
+                self.bs_train_data, self.svm_evaluate_data = train_test_split(self.data, test_size=0.90, stratify=self.data[:, -1])
+            else:
+                self.bs_train_data, self.svm_evaluate_data = train_test_split(self.data, test_size=0.90)
 
     def is_classification(self):
         return DSManager.is_dataset_classification(self.name)
@@ -30,17 +39,17 @@ class DSManager:
 
     def get_a_fold(self, seed=50):
         if self.is_classification():
-            return train_test_split(self.data[:,0:-1], self.data[:,-1], test_size=0.95, random_state=seed, stratify=self.data[:, -1])
-        return train_test_split(self.data[:, 0:-1], self.data[:, -1], test_size=0.95, random_state=seed)
+            return train_test_split(self.svm_evaluate_data[:,0:-1], self.svm_evaluate_data[:,-1], test_size=0.95, random_state=seed, stratify=self.svm_evaluate_data[:, -1])
+        return train_test_split(self.svm_evaluate_data[:, 0:-1], self.svm_evaluate_data[:, -1], test_size=0.95, random_state=seed)
 
     def get_bs_train_x_y(self):
         return self.get_bs_train_x(), self.get_bs_train_y()
 
     def get_bs_train_x(self):
-        return self.data[:,0:-1]
+        return self.bs_train_data[:,0:-1]
 
     def get_bs_train_y(self):
-        return self.data[:, -1]
+        return self.bs_train_data[:, -1]
 
     def __repr__(self):
         return self.get_name()
