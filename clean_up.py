0d8ff4ed72b9c9e1b8e43a8ffa57dc687d040bdb
Arif <arif@themascoteers.com>
2024-10-29 13:37:05 +1100

    done

---
+++ clean_up.py
@@ -0,0 +1,12 @@
+import shutil
+import os
+
+
+def delete_all_files(folder_path):
+    shutil.rmtree(folder_path)
+    os.makedirs(folder_path)
+
+
+delete_all_files("saved_graphics")
+delete_all_files("results")
+    
