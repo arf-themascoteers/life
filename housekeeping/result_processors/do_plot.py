74c705271e3c473e9cc3b356f60b19115b3c38f0
Arif <arif@themascoteers.com>
2024-10-29 11:22:38 +1100

    done

---
+++ housekeeping/result_processors/do_plot.py
@@ -0,0 +1,10 @@
+import plotter_regression
+import plotter_classification
+
+
+def plot(*args, **kwargs):
+    plotter_classification.plot_combined(*args, **kwargs)
+    plotter_regression.plot_combined(*args, **kwargs)
+
+
+plot(only_algorithms=["bsdrattn","skipattn","lateattn","stage3attn"])
