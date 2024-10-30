5d142dbb8d5137a85daac17a233478d1bdaa2822
Arif <arif@themascoteers.com>
2024-10-30 23:23:11 +1100

    done

---
+++ run_bsdr3.py
@@ -0,0 +1,12 @@
+from task_runner import TaskRunner
+
+
+if __name__ == '__main__':
+    tag = "bsdr3"
+    tasks = {
+        "algorithms" : ["bsdr3"],
+        "datasets": ["indian_pines"],
+        "target_sizes" : list(range(2,31))
+    }
+    ev = TaskRunner(tasks,tag,skip_all_bands=True, verbose=False, test=False)
+    summary, details = ev.evaluate()
