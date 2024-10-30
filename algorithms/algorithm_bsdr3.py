d79d82547c80646054f346ef75613a748baa4b56
Arif <arif@themascoteers.com>
2024-10-30 23:23:51 +1100

    done

--- algorithms/algorithm_bsdr3.py
+++ algorithms/algorithm_bsdr3.py
@@ -3,9 +3,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 from train_test_evaluator import evaluate_split
-from torch.jit import fork, wait
-import math
-from itertools import accumulate
 
 
 def r2_score_torch(y, y_pred):
@@ -38,137 +35,48 @@ class LinearInterpolationModule(nn.Module):
         return y_interpolated
 
 
-class Agent(nn.Module):
-    def __init__(self, target_size, class_size, classification, offset, start, r1, r2, last=False):
+class ANN(nn.Module):
+    def __init__(self, target_size, class_size):
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.target_size = target_size
         self.class_size = class_size
-        self.classification = classification
-        self.last = last
-        self.r1 = torch.tensor(r1, dtype=torch.float32)
-        self.r2 = torch.tensor(r2, dtype=torch.float32)
+
         init_vals = torch.linspace(0.001, 0.99, self.target_size + 2)
-        displacement = offset*start
-        init_vals[1:-1] = init_vals[1:-1] + displacement
-        init_vals = torch.clamp(init_vals, max=0.99)
-        self.raw_indices = nn.Parameter(
-            torch.tensor([Agent.inverse_sigmoid_torch(init_vals[i + 1]) for i in range(self.target_size)],
+        self.indices = nn.Parameter(
+            torch.tensor([ANN.inverse_sigmoid_torch(init_vals[i + 1]) for i in range(self.target_size)],
                          requires_grad=True).to(self.device))
         self.linear = nn.Sequential(
             nn.Linear(self.target_size, 128),
-            nn.LeakyReLU(),
-            nn.Linear(128, 64),
-            nn.LeakyReLU(),
-            nn.Linear(64, self.class_size)
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, self.class_size)
         )
-        # print(torch.min(self.indices))
-        # print(torch.max(self.indices))
-        if self.classification:
-            self.criterion = torch.nn.CrossEntropyLoss()
-        else:
-            self.criterion = torch.nn.MSELoss()
+        num_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        print("Number of learnable parameters:", num_params)
 
     @staticmethod
     def inverse_sigmoid_torch(x):
         return -torch.log(1.0 / x - 1.0)
 
-    def forward(self, linterp, y):
-        indices = self.get_indices()
-        outputs = linterp(indices)
+    def forward(self, linterp):
+        outputs = linterp(self.get_indices())
         soc_hat = self.linear(outputs)
         if self.class_size == 1:
             soc_hat = soc_hat.reshape(-1)
-        loss = self.criterion(soc_hat,y)
-
-        norm = torch.norm(indices, p=2)
-        r1_loss = torch.relu(self.r1 - norm)
-        r_loss = r1_loss
-        if not self.last:
-            r2_loss = torch.relu(norm - self.r2)
-            r_loss = r_loss + r2_loss
-
-        successive_loss = torch.sum(torch.stack([torch.relu(self.raw_indices[i] - self.raw_indices[i + 1]) for i in range(len(self.raw_indices) - 1)]))
-        return soc_hat, loss, r_loss, successive_loss
+        return soc_hat
 
     def get_indices(self):
-        return torch.sigmoid(self.raw_indices)
+        return torch.sigmoid(self.indices)
 
 
-class ANN(nn.Module):
-    def __init__(self, target_size, class_size, original_size, classification):
-        super().__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.target_size = target_size
-        self.class_size = class_size
-        self.original_size = original_size
-        self.num_agents = 10
-        self.classification = classification
-
-        band_unit = 1 / original_size
-
-        rs = [0]+self.get_rs()
-        self.agents = nn.ModuleList(
-            [
-                Agent(self.target_size, self.class_size, self.classification, band_unit, i, rs[i], rs[i+1], last=(i==self.num_agents-1)) for i in range(self.num_agents)
-            ]
-        )
-
-        self.best = 0
-
-    def print_weights(self):
-        for i in range(self.num_agents):
-            print("\t".join([str(round(i.item(),5)) for i in self.agents[i].get_indices()]))
-            print("\t".join([str(round(i.item(), 5)) for i in self.agents[i].get_indices()*self.original_size]))
-
-    def get_rs(self):
-        r_values = [1/math.pow(1+1,2) for i in range(self.num_agents)]
-        s = sum(r_values)
-        r_values = [r / s for r in r_values]
-        r_values = list(accumulate(r_values))
-        r_values = [r*math.sqrt(self.target_size) for r in r_values]
-        return r_values
-
-    def forward(self, linterp, y):
-        futures = [fork(linear, linterp, y) for linear in self.agents]
-        y_preds, losses, r_losses, successive_loss= zip(*[wait(future) for future in futures])
-        losses = torch.stack(losses)
-        r_losses = torch.stack(r_losses)
-        successive_loss = torch.stack(successive_loss)
-        self.best = torch.argmin(losses)
-        output = y_preds[self.best]
-        loss = torch.sum(losses) + 40*torch.sum(r_losses) + 40*torch.sum(successive_loss)
-
-        if False:
-            ls = [str(round(l.item(),5)) for l in losses]
-            ls = "\t".join(ls)
-
-            rs = [str(round(l.item(),5)) for l in r_losses]
-            rs = "\t".join(rs)
-
-            ss = [str(round(l.item(),5)) for l in successive_loss]
-            ss = "\t".join(ss)
-
-            s = ls + "\t\t" + rs + "\t\t" + ss + "\t\t"
-
-            agent_bands = [a.get_indices()*self.original_size for a in self.agents]
-            band_strs = []
-            for ab in agent_bands:
-                band_strs.append("\t".join([str(round(b.item())) for b in ab]))
-
-            band_strs = "\t\t".join(band_strs)
-
-            s= s + "\t\t" + band_strs
-
-            print(s)
-
-        return output, loss
-
-    def get_ann_indices(self):
-        return self.agents[self.best].get_indices()
-
-
-class Algorithm_msobsdr2(Algorithm):
+class Algorithm_bsdr3(Algorithm):
     def __init__(self, target_size, dataset, tag, reporter, verbose, test):
         super().__init__(target_size, dataset, tag, reporter, verbose, test)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -179,17 +87,19 @@ class Algorithm_msobsdr2(Algorithm):
         self.target_size = target_size
         self.classification = dataset.is_classification()
         if self.classification:
+            self.criterion = torch.nn.CrossEntropyLoss()
             self.class_size = len(np.unique(self.dataset.get_bs_train_y()))
             self.lr = 0.01
-            self.total_epoch = 1000
+            self.total_epoch = 500
         else:
+            self.criterion = torch.nn.MSELoss()
             self.class_size = 1
             self.lr = 0.001
-            self.total_epoch = 1000
-        self.original_feature_size = self.dataset.get_bs_train_x().shape[1]
-        self.ann = ANN(self.target_size, self.class_size, self.original_feature_size, self.classification)
-        self.ann.to(self.device)
+            self.total_epoch = 500
 
+        self.ann = ANN(self.target_size, self.class_size)
+        self.ann.to(self.device)
+        self.original_feature_size = self.dataset.get_bs_train_x().shape[1]
 
         self.X_train = torch.tensor(self.dataset.get_bs_train_x(), dtype=torch.float32).to(self.device)
         ytype = torch.float32
@@ -208,7 +118,10 @@ class Algorithm_msobsdr2(Algorithm):
             y = self.y_train
         for epoch in range(self.total_epoch):
             optimizer.zero_grad()
-            y_hat, loss = self.ann(linterp, y)
+            y_hat = self.ann(linterp)
+            if not self.classification:
+                y_hat = y_hat.reshape(-1)
+            loss = self.criterion(y_hat, y)
             loss.backward()
             optimizer.step()
             r2_train = 0
@@ -250,10 +163,7 @@ class Algorithm_msobsdr2(Algorithm):
         print("".join([str(i).ljust(20) for i in cells]))
 
     def get_indices(self):
-        indices = self.ann.get_ann_indices().cpu().detach()
-        indices = torch.round(indices * self.original_feature_size).to(torch.int64)
-        indices = torch.where(indices < 0, torch.tensor(0), indices)
-        indices = torch.where(indices > self.original_feature_size-1, torch.tensor(self.original_feature_size-1), indices)
+        indices = torch.round(self.ann.get_indices() * self.original_feature_size ).to(torch.int64).tolist()
         return list(dict.fromkeys(indices))
 
     def transform(self, X):
