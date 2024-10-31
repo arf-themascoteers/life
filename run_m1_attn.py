6e41e9ac0faf9de9bbef5d3d57d54cb31c913e43
arif <arif@themascoteers.com>
2024-10-31 19:27:31 +1100

    done

--- run_m1_attn.py
+++ run_m1_attn.py
@@ -3,7 +3,7 @@ from task_runner import TaskRunner
 if __name__ == '__main__':
     tag = "m1"
     tasks = {
-        "algorithms" : ["bsdr","c1","bsdrattn"],
+        "algorithms" : ["bsdr","c1","bsdrattn2","skipattn2"],
         "datasets": ["indian_pines","paviaU","salinas","ghisaconus","lucas_r"],
         "target_sizes" : list(range(2,151))
     }
