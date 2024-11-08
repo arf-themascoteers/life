60e2b64b83750cc6eda8cd016278b070574dbded
Arif <arif@themascoteers.com>
2024-11-09 00:41:53 +1100

    done

--- housekeeping/check_svr_lucas.py
+++ housekeeping/check_svr_lucas.py
@@ -1,4 +1,3 @@
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.metrics import r2_score, mean_squared_error
@@ -6,13 +5,18 @@ import numpy as np
 from sklearn.svm import SVR
 
 data = pd.read_csv("../data/lucas_r.csv")
+_, data = train_test_split(data, test_size=0.1, random_state=42)
 
-idx = [i for i in range(100,4100,10)]
+#idx = [i for i in range(200,4100,20)]
+idx = [i for i in range(0,4200,1)]
+
+print(len(idx))
 
 X = data.iloc[:, idx]
 y = data.iloc[:, -1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
 model = SVR(C=100, kernel='rbf', gamma=1)
 model.fit(X_train, y_train)
@@ -22,3 +26,4 @@ r2 = r2_score(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
 print(r2, rmse)
+
