43f5b27abacbc98aa5069b9b2407cbe556c60849
Arif <arif@themascoteers.com>
2024-11-11 03:13:40 +1100

    done

--- run_p22_health.py
+++ run_p22_health.py
@@ -1,11 +1,15 @@
 from task_runner import TaskRunner
+import plotter_classification
+import clean_up
 
 if __name__ == '__main__':
+    clean_up.do_it()
     tag = "p22"
     tasks = {
         "algorithms" : ["c2"],
         "datasets": ["ghisaconus_health"],
         "target_sizes" : list(range(2,31))
     }
-    ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
+    ev = TaskRunner(tasks,tag,skip_all_bands=True, verbose=True, test=False)
     summary, details = ev.evaluate()
+    plotter_classification.plot_combined(sources=["p21","p22"],only_algorithms=["bsnet","bnc","c1","c2"], only_datasets=["ghisaconus_health"])
