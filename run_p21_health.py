2b344147d8e4065051c8635996e00d9107cc089d
Arif <arif@themascoteers.com>
2024-11-10 19:25:37 +1100

    done

---
+++ run_p21_health.py
@@ -0,0 +1,11 @@
+from task_runner import TaskRunner
+
+if __name__ == '__main__':
+    tag = "p6"
+    tasks = {
+        "algorithms" : ["bsnet", "bnc", "c1"],
+        "datasets": ["ghisaconus_health"],
+        "target_sizes" : list(range(2,31))
+    }
+    ev = TaskRunner(tasks,tag,skip_all_bands=False, verbose=False, test=False)
+    summary, details = ev.evaluate()
