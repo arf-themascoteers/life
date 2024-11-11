8d5fe6b845b71e1011e1c60eb989b072e1f2fcb5
Arif <arif@themascoteers.com>
2024-11-12 01:42:32 +1100

    Ignore

--- do_plot.py
+++ do_plot.py
@@ -7,4 +7,8 @@ def plot(*args, **kwargs):
     plotter_regression.plot_combined(*args, **kwargs)
 
 
-plotter_classification.plot_combined(sources=["p21","p22"],only_algorithms=["bsnet","bnc","c1","c2"], only_datasets=["ghisaconus_health"])
+plotter_classification.plot_combined(
+    sources=["p21","p22","p23"],
+    only_algorithms=["bsnet","bnc","c1","c3"],
+    only_datasets=["ghisaconus_health"]
+)
